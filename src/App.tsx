56332790e8ca2c4f373c967d3fe02c41762343ab
arifweb2020 <arifhussainb2019@gmail.com>
2023-10-31 16:48:03 +0530

    webpack for dev and prod environment

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div>
       <h1>Wbpack setup</h1>
+      <p>Env testing - {process.env.NODE_ENV}</p>
+      <p>own env var - {process.env.name}</p>
       <img src={Logo} alt="logo" width="240"/>
     </div>
   );
