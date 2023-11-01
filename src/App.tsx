f41f0dad1b958cf36d80f845c763d51eec24fba7
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-01 12:21:31 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Counter from './Counter'
 
 const App = () => {
   const name = 'Arif'
+  const num = 1
   return (
     <div>
-      <h1>Webpack setup refresh - {name}</h1>
+      <h1>
+        Webpack setup refresh - {name} - {num}
+      </h1>
       <p>Env testing - {process.env.NODE_ENV}</p>
       <p>own env var - {process.env.name}</p>
       <img src={Logo} alt="logo" width="240" />
